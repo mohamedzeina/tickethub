ff40c3aca59f2360e28507855f982aacfc8197c8
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-10 16:10:32 +0300

    Add ingress config and current user route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('api/users/currentuser', (req, res) => {
+	res.send('Current user route');
+});
+
 app.listen(3000, () => {
 	console.log('Auth service is running on port 3000');
 });
