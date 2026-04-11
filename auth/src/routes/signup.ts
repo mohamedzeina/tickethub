e76b26e7d2836004f8a0be7cbdb2a218c57842cb
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-11 22:49:25 +0300

    Handle validation errors in user signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,23 +1,30 @@
-import express, {  Request, Response } from 'express';
-import { body } from 'express-validator';
-
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-  body('email')
-  .isEmail()
-  .withMessage('Email must be valid'),
-  body('password')
-  .trim()
-  .isLength({ min: 4, max: 20})
-  .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
-	const { email, password } = req.body; 
+router.post(
+	'/api/users/signup',
+	[
+		body('email').isEmail().withMessage('Email must be valid'),
+		body('password')
+			.trim()
+			.isLength({ min: 4, max: 20 })
+			.withMessage('Password must be between 4 and 20 characters'),
+	],
+	(req: Request, res: Response) => {
+		const errors = validationResult(req);
 
-  if(!email || typeof email !== 'string') {
-    res.status(400).send({'Provide a valid email'})
-  }
-});
+		if (!errors.isEmpty()) {
+			return res.status(400).send({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		console.log('Creating user with email:', email, 'and password:', password);
+
+		res.send({});
+	},
+);
 
 export { router as signUpRouter };
