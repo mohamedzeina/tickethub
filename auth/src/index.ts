5d81b73fd994d07469cc27c11fa81f4974a5c03e
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-11 22:23:48 +0300

    Add routes folder for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (_req, res) => {
-	res.send('Current user route');
-});
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
 	console.log('Auth service is running on port 3000');
