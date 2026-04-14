63d6cd256c31051fc0532bda9876756c8fdaa146
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-14 17:15:04 +0300

    feat(auth): implement user signin
    Authenticate users by verifying email/password, issue a JWT, and store it in the session.

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
+import { PasswordManager } from '../services/password';
+import { User } from '../models/user';
 import { validateRequest } from '../middlewares/validateRequest';
+import { BadRequestError } from '../errors/badRequestError';
 
 const router = express.Router();
 
@@ -12,7 +16,38 @@ router.post(
 		body('password').trim().notEmpty().withMessage('Password is required'),
 	],
 	validateRequest,
-	(req: Request, res: Response) => {},
+	async (req: Request, res: Response) => {
+		const { email, password } = req.body;
+
+		const existingUser = await User.findOne({ email });
+		if (!existingUser) {
+			throw new BadRequestError('Invalid credentials');
+		}
+
+		const passwordsMatch = await PasswordManager.compare(
+			existingUser.password,
+			password,
+		);
+		if (!passwordsMatch) {
+			throw new BadRequestError('Invalid credentials');
+		}
+
+		// Generate JWT
+		const userJWT = jwt.sign(
+			{
+				id: existingUser.id,
+				email: existingUser.email,
+			},
+			process.env.JWT_KEY!,
+		);
+
+		// Store it on session object
+		req.session = {
+			jwt: userJWT,
+		};
+
+		res.status(200).send(existingUser);
+	},
 );
 
 export { router as signInRouter };
