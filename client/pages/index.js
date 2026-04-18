2c734b702afba292c67346e4c22d0e4b1d489e4f
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-18 21:12:28 +0300

    feat(client): Fetch current user in getInitialProps with SSR/browser routing
    Use ingress URL on the server and relative path on the browser to handle both environments.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,35 @@
-const LandingPage = () => {
-	return (
-		<div>
-			<h1>Welcome to the Landing Page</h1>
-			<p>This is the main entry point of the application.</p>
-		</div>
-	);
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+	console.log(currentUser);
+	// axios.get('/api/users/currentuser').catch((err) => {
+	// 	console.log(err.message);
+	// });
+
+	return <h1>Landing Page</h1>;
+};
+
+// This function runs on the server during the initial page load, and also on the
+// client during client-side navigation. It allows us to fetch data and pass it
+// as props to the component.
+LandingPage.getInitialProps = async () => {
+	if (typeof window === 'undefined') {
+		// We are on the server, make request to
+		// http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+		const { data } = await axios.get(
+			'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+			{
+				headers: {
+					host: 'tickethub.com',
+				},
+			},
+		);
+		return data;
+	} else {
+		// We are on the browser, make request to /api/users/currentuser
+		const { data } = await axios.get('/api/users/currentuser');
+		return data;
+	}
 };
 
 export default LandingPage;
