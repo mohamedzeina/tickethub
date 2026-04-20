724a9e5225cdd7be21d8ef9c1d39b53779f56018
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-20 20:39:56 +0300

    test(tickets): Add tests for new ticket creation route

---
+++ tickets/src/__tests__/new.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest';
+import { app } from '../app';
+
+it('has a route handler listening to /api/tickets for post request', async () => {});
+
+it('can only be accessed by logged-in users', async () => {});
+
+it('returns an error if an invalid title is provided', async () => {});
+
+it('returns an error if an invalid price is provided', async () => {});
+
+it('creates a ticket if valid title and price are provided', async () => {});
