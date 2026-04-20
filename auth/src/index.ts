d05f16c343fde89411395ce769c147b0941c2895
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-20 20:29:44 +0300

    refactor(infra): Move MongoDB URIs to env vars in auth and tickets services

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,10 +6,13 @@ const startAuthService = async () => {
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
 	}
+	if (!process.env.MONGO_URI) {
+		throw new Error('MONGO_URI must be defined');
+	}
 
 	try {
-		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-		console.log('Connected to MongoDB');
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log('Connected to Auth MongoDB');
 	} catch (err) {
 		console.error(err);
 	}
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,16 +5,19 @@ const startAuthService = async () => {
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
 	}
+	if (!process.env.MONGO_URI) {
+		throw new Error('MONGO_URI must be defined');
+	}
 
 	try {
-		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-		console.log('Connected to MongoDB');
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log('Connected to Tickets MongoDB');
 	} catch (err) {
 		console.error(err);
 	}
 
 	app.listen(3000, () => {
-		console.log('Auth service is running on port 3000');
+		console.log('Tickets service is running on port 3000');
 	});
 };
 
