948d0f28f7248d1d22cacb5f14e668a363e501f5
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-20 15:57:20 +0300

    feat(client): Render dynamic nav links in header based on auth state

--- client/components/header.js
+++ client/components/header.js
@@ -1,5 +1,21 @@
 import Link from 'next/link';
 const Header = ({ currentUser }) => {
+	const links = [
+		!currentUser && { label: 'Sign Up', href: '/auth/signup' },
+		!currentUser && { label: 'Sign In', href: '/auth/signin' },
+		currentUser && { label: 'Sign Out', href: '/auth/signout' },
+	]
+		.filter((linkConfig) => linkConfig) // Filter out any falsy values (like false or null)
+		.map(({ label, href }) => {
+			return (
+				<li key={href} className="nav-item">
+					<Link className="navbar-brand" href={href}>
+						{label}
+					</Link>
+				</li>
+			);
+		});
+
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<Link className="navbar-brand" href="/">
@@ -7,9 +23,7 @@ const Header = ({ currentUser }) => {
 			</Link>
 
 			<div className="d-flex justify-content-end">
-				<ul className="nav d-flex allign-items-center">
-					{currentUser ? 'Sign out' : 'Sign in'}
-				</ul>
+				<ul className="nav d-flex allign-items-center">{links}</ul>
 			</div>
 		</nav>
 	);
