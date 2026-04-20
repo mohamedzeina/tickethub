b3917b3b124e71566b03898a8b1a8efa0b485618
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-20 15:38:00 +0300

    feat(client): Fetch current user in _app getInitialProps and propagate page props

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import buildClient from '../api/buildClient';
 
 // This file is used to initialize pages. We are adding this to have a global
 // CSS import for Bootstrap, which is required for styling our application.
 // By importing the Bootstrap CSS here, it will be available across all pages
 // in the application without needing to import it in each individual page component.
 
-export default ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+	return (
+		<div>
+			<Component {...pageProps} />;
+		</div>
+	);
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+	const client = buildClient(appContext.ctx);
+	const { data } = await client.get('api/users/currentuser').catch((err) => {
+		console.log(err.message);
+	});
+
+	// We need to call the getInitialProps of the individual page component to fetch
+	// any data that it needs. This is important because some pages might have
+	// their own getInitialProps to fetch specific data, and we want to make sure
+	// that still works.
+	let pageProps = {};
+	if (appContext.Component.getInitialProps) {
+		pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+	}
+
+	return data;
+};
+
+export default AppComponent;
