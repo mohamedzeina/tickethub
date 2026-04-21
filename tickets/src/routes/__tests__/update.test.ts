e57b2b2298ada2f208c40bcc5e1aae82327293ca
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-22 00:38:16 +0300

    feat(tickets): Complete update route with validation and persistence

--- tickets/src/routes/__tests__/update.test.ts
+++ tickets/src/routes/__tests__/update.test.ts
@@ -53,6 +53,65 @@ it('returns a 401 if the user does not own the ticket', async () => {
 	expect(ticketResponse.body.price).toEqual(response.body.price);
 });
 
-it('returns a 400 if the user provides an invalid title or price', async () => {});
+it('returns a 400 if the user provides an invalid title or price', async () => {
+	const cookie = global.signin();
 
-it('updates the ticket provided valid inputs', async () => {});
+	const response = await request(app)
+		.post('/api/tickets')
+		.set('Cookie', cookie)
+		.send({
+			title: 'Test title',
+			price: 20,
+		})
+		.expect(201);
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({
+			title: '',
+			price: 20,
+		})
+		.expect(400);
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({
+			title: 'Test title',
+			price: -10,
+		})
+		.expect(400);
+});
+
+it('updates the ticket provided valid inputs', async () => {
+	const cookie = global.signin();
+
+	const response = await request(app)
+		.post('/api/tickets')
+		.set('Cookie', cookie)
+		.send({
+			title: 'Test title',
+			price: 20,
+		})
+		.expect(201);
+
+	const newTitle = 'New title';
+	const newPrice = 40;
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({
+			title: newTitle,
+			price: newPrice,
+		})
+		.expect(200);
+
+	const ticketResponse = await request(app)
+		.get(`/api/tickets/${response.body.id}`)
+		.send({});
+
+	expect(ticketResponse.body.title).toEqual(newTitle);
+	expect(ticketResponse.body.price).toEqual(newPrice);
+});
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -15,6 +15,13 @@ const router = express.Router();
 router.put(
 	'/api/tickets/:id',
 	requireAuth,
+	[
+		body('title').not().isEmpty().withMessage('Title is required'),
+		body('price')
+			.isFloat({ gt: 0 })
+			.withMessage('Price must be greater than 0'),
+	],
+	validateRequest,
 	async (req: Request, res: Response) => {
 		const ticket = await Ticket.findById(req.params.id);
 
@@ -25,6 +32,14 @@ router.put(
 		if (ticket.userId !== req.currentUser!.id) {
 			throw new NotAuthorizedError();
 		}
+
+		ticket.set({
+			title: req.body.title,
+			price: req.body.price,
+		});
+
+		await ticket.save();
+
 		res.send(ticket);
 	},
 );
