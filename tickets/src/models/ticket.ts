6cb405a5b776b452dd0d90cbae6b4676bb4f1364
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-21 20:38:04 +0300

    feat(tickets): Implement Ticket mongoose schema and build static

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+//
 interface TicketAttrs {
 	title: string;
 	price: number;
@@ -12,6 +13,48 @@ interface TicketDoc extends mongoose.Document {
 	userId: string;
 }
 
+// Interface for the JSON representation after transformation
+interface TicketJSON {
+	title: String;
+	_id?: mongoose.Types.ObjectId; // Optional for deletion
+	price: Number;
+	userId: String;
+	id?: string; // Added during transformation
+}
+
 interface TicketModel extends mongoose.Model<TicketDoc> {
 	build(attrs: TicketAttrs): TicketDoc;
 }
+
+const ticketSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		userId: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		toJSON: {
+			transform(doc: TicketDoc, ret: TicketJSON) {
+				ret.id = ret._id?.toString();
+				delete ret._id;
+			},
+		},
+	},
+);
+
+ticketSchema.statics.build = (attr: TicketAttrs) => {
+	return new Ticket(attr);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
