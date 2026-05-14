4230bf349494673219c9141e9a67a9e164aa83ab
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-14 22:30:38 +0300

    feat(nats): Enable manual ack mode in subscriber to confirm message processing

--- nats-test/src/subscriber.ts
+++ nats-test/src/subscriber.ts
@@ -10,9 +10,12 @@ const stan: any = nats.connect('tickethub', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
 	console.log('Subscriber connected to NATS');
 
+	const options = stan.subscriptionOptions().setManualAckMode(true);
+
 	const subscription = stan.subscribe(
 		'ticket:created',
 		'subscription-queue-group',
+		options,
 	);
 	subscription.on('message', (msg: Message) => {
 		const data = msg.getData();
@@ -20,5 +23,7 @@ stan.on('connect', () => {
 		if (typeof data === 'string') {
 			console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
 		}
+
+		msg.ack();
 	});
 });
