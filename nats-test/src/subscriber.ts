373382a3038ad1ac702974575a0369b7cfab6acf
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-14 22:56:12 +0300

    feat(nats): Gracefully close subscriber connection on SIGINT and SIGTERM

--- nats-test/src/subscriber.ts
+++ nats-test/src/subscriber.ts
@@ -10,6 +10,11 @@ const stan: any = nats.connect('tickethub', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
 	console.log('Subscriber connected to NATS');
 
+	stan.on('close', () => {
+		console.log('NATS connection closed!');
+		process.exit();
+	});
+
 	const options = stan.subscriptionOptions().setManualAckMode(true);
 
 	const subscription = stan.subscribe(
@@ -27,3 +32,6 @@ stan.on('connect', () => {
 		msg.ack();
 	});
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
