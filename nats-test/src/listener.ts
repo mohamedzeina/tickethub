d9e22e5db591ec6b9a91918ec493a86e38a47e42
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-15 21:29:48 +0300

    refactor(nats): Replace subscriber script with listener entry point

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,26 +15,7 @@ stan.on('connect', () => {
 		process.exit();
 	});
 
-	const options = stan
-		.subscriptionOptions()
-		.setManualAckMode(true)
-		.setDeliverAllAvailable()
-		.setDurableName('ticket-service');
-
-	const subscription = stan.subscribe(
-		'ticket:created',
-		'ticket-service-group',
-		options,
-	);
-	subscription.on('message', (msg: Message) => {
-		const data = msg.getData();
-
-		if (typeof data === 'string') {
-			console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-		}
-
-		msg.ack();
-	});
+	new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -80,5 +61,16 @@ abstract class Listener {
 		return typeof data === 'string'
 			? JSON.parse(data)
 			: JSON.parse(data.toString('utf8'));
+	}
+}
+
+class TicketCreatedListener extends Listener {
+	subject = 'ticket:created';
+	queueGroupName = 'payment-service';
+
+	onMessage(data: any, msg: Message) {
+		console.log('Event data:', data);
+
+		msg.ack();
 	}
 }
