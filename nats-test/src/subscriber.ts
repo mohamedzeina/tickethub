4612df95406f6fa944f1f07f2153a5997b75fe69
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-15 21:17:12 +0300

    feat(nats): Add abstract Listener base class to encapsulate subscription logic

--- nats-test/src/subscriber.ts
+++ nats-test/src/subscriber.ts
@@ -1,9 +1,9 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan: any = nats.connect('tickethub', randomBytes(4).toString('hex'), {
+const stan: Stan = nats.connect('tickethub', randomBytes(4).toString('hex'), {
 	url: 'http://localhost:4222',
 });
 
@@ -39,3 +39,46 @@ stan.on('connect', () => {
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+abstract class Listener {
+	abstract subject: string;
+	abstract queueGroupName: string;
+	abstract onMessage(data: any, msg: Message): void;
+	private client: Stan;
+	private ackWait = 5 * 1000;
+
+	constructor(client: Stan) {
+		this.client = client;
+	}
+
+	subscriptionOptions() {
+		return this.client
+			.subscriptionOptions()
+			.setManualAckMode(true)
+			.setAckWait(this.ackWait)
+			.setDeliverAllAvailable()
+			.setDurableName(this.queueGroupName);
+	}
+
+	listen() {
+		const subscription = this.client.subscribe(
+			this.subject,
+			this.queueGroupName,
+			this.subscriptionOptions(),
+		);
+
+		subscription.on('message', (msg: Message) => {
+			console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+			const parsedData = this.parseMessage(msg);
+			this.onMessage(parsedData, msg);
+			msg.ack();
+		});
+	}
+
+	parseMessage(msg: Message) {
+		const data = msg.getData();
+		return typeof data === 'string'
+			? JSON.parse(data)
+			: JSON.parse(data.toString('utf8'));
+	}
+}
