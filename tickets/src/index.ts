6c00ae7539c2fcf259e1e9c4c6051dfa13c580f7
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-17 18:33:23 +0300

    feat(tickets): Close NATS connection on SIGINT/SIGTERM

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -17,6 +17,14 @@ const startAuthService = async () => {
 			'http://nats-srv:4222',
 		);
 
+		natsWrapper.client.on('close', () => {
+			console.log('NATS connection closed!');
+			process.exit();
+		});
+
+		process.on('SIGINT', () => natsWrapper.client.close());
+		process.on('SIGTERM', () => natsWrapper.client.close());
+
 		await mongoose.connect(process.env.MONGO_URI);
 		console.log('Connected to Tickets MongoDB');
 	} catch (err) {
