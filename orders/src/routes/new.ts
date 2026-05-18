967db91b7dd0f827c243ac71279cd729f73154b8
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-18 18:55:39 +0300

    feat(orders): Reject new orders when ticket is already reserved

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@zeina-tickethub/common';
+import {
+	NotFoundError,
+	requireAuth,
+	validateRequest,
+	OrderStatus,
+	BadRequestError,
+} from '@zeina-tickethub/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -10,6 +18,39 @@ router.post(
 	[body('ticketId').not().isEmpty().withMessage('ticketId must be provided')],
 	validateRequest,
 	async (req: Request, res: Response) => {
+		const { ticketId } = req.body;
+
+		// Find the ticket the user is trying to order in the database
+		const ticket = await Ticket.findById(ticketId);
+		if (!ticket) {
+			throw new NotFoundError();
+		}
+
+		// Make sure the ticket is not already reserved
+		// Run query to look at all orders. Find an order where the ticket
+		// is the ticket we just found *and* the orders status is *not* cancelled
+		// If we find an order from that means the ticket *is* reserved
+		const existingOrder = await Order.findOne({
+			ticket: ticket,
+			status: {
+				$in: [
+					OrderStatus.Created,
+					OrderStatus.AwaitingPayment,
+					OrderStatus.Complete,
+				],
+			},
+		});
+		if (existingOrder) {
+			throw new BadRequestError('Ticket is already reserved');
+		}
+
+		// Calculate an expiration date for this order
+
+		// Build the order and save it to the database
+
+		// Publish an event saying that an order was created
+
+		// Return the order to the client
 		res.send({});
 	},
 );
