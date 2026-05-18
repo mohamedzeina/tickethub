434a245d40ae3ef83d1d70742db047a9453f316f
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-18 20:15:56 +0300

    feat(orders): Return current user's orders with populated tickets

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, NotAuthorizedError } from '@zeina-tickethub/common';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-	res.send({});
-});
+router.get(
+	'/api/orders/:orderId',
+	requireAuth,
+	async (req: Request, res: Response) => {
+		const orders = await Order.find({
+			userId: req.currentUser?.id,
+		}).populate('ticket');
+
+		res.status(200).send(orders);
+	},
+);
 
 export { router as showOrderRouter };
