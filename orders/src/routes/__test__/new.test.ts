16a4c64029680671c17de45e057f7d08396895a9
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-18 20:01:21 +0300

    test(orders): Implement reserved-ticket rejection test

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
 
 it('returns an error if the ticket does not exist', async () => {
 	const ticketId = new mongoose.Types.ObjectId();
@@ -14,5 +16,28 @@ it('returns an error if the ticket does not exist', async () => {
 		.expect(404);
 });
 
-it('returns an error if the ticket is already reserved', async () => {});
+it('returns an error if the ticket is already reserved', async () => {
+	const ticket = Ticket.build({
+		title: 'Akon Concert',
+		price: 100,
+	});
+	await ticket.save();
+
+	const order = Order.build({
+		userId: 'asfafafas',
+		ticket,
+		status: OrderStatus.Created,
+		expiresAt: new Date(),
+	});
+	await order.save();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({
+			ticketId: ticket.id,
+		})
+		.expect(400);
+});
+
 it('reserves a ticket', async () => {});
