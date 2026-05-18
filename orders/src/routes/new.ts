8508cde51bef38e0acfb476f1b2789b9f49a9d53
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-18 17:15:17 +0300

    feat(orders): Require auth and non-empty ticketId for new order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@zeina-tickethub/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-	res.send({});
-});
+router.post(
+	'/api/orders',
+	requireAuth,
+	[body('ticketId').not().isEmpty().withMessage('ticketId must be provided')],
+	validateRequest,
+	async (req: Request, res: Response) => {
+		res.send({});
+	},
+);
 
 export { router as newOrderRouter };
