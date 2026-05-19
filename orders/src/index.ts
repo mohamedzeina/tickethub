81f207407c0800fabfdebb2f960541caf790f422
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-19 20:32:12 +0300

    feat(orders): Start ticket created and updated listeners

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const startAuthService = async () => {
 	if (!process.env.JWT_KEY) {
@@ -37,6 +39,9 @@ const startAuthService = async () => {
 
 		process.on('SIGINT', () => natsWrapper.client.close());
 		process.on('SIGTERM', () => natsWrapper.client.close());
+
+		new TicketCreatedListener(natsWrapper.client).listen();
+		new TicketUpdatedListener(natsWrapper.client).listen();
 
 		await mongoose.connect(process.env.MONGO_URI);
 		console.log('Connected to Orders MongoDB');
