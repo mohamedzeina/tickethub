ff2cd9b2ab81216cd0968a7267c487fb70e3a399
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-19 14:45:36 +0300

    test(orders): Assert order status is canceled after delete

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 import { Ticket } from '../../models/ticket';
+import { OrderStatus } from '@zeina-tickethub/common';
 
 it('cancels the order', async () => {
 	const ticket = Ticket.build({
@@ -20,7 +21,16 @@ it('cancels the order', async () => {
 	await request(app)
 		.delete(`/api/orders/${order.id}`)
 		.set('Cookie', user)
+		.send()
 		.expect(204);
+
+	const { body: canceledOrder } = await request(app)
+		.get(`/api/orders/${order.id}`)
+		.set('Cookie', user)
+		.send()
+		.expect(200);
+
+	expect(canceledOrder!.status).toEqual(OrderStatus.Canceled);
 });
 
 it('returns unauthorized error if user tries to cancel an order that does not belong to them', async () => {
