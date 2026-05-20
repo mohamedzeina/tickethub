ce2f394098f3b79a397c9d08e658e991cd5d3220
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-20 14:39:10 +0300

    feat(common): Add version field to ticket and order events
    Publish common 1.0.25, bump consumers, and add a test that the Ticket
    version increments on each save.

--- tickets/src/models/__tests__/ticket.test.ts
+++ tickets/src/models/__tests__/ticket.test.ts
@@ -31,3 +31,32 @@ it('implements optimistic concurrency control', async () => {
 
 	throw new Error('Should not reach this point');
 });
+
+it('increments the version number on multiple saves', async () => {
+	// Create an instance of a ticket
+	const ticket = Ticket.build({
+		title: 'Akon Concert',
+		price: 50,
+		userId: '123',
+	});
+
+	// Save the ticket to the database
+	await ticket.save();
+
+	// Expect the version of the ticket to be 0
+	expect(ticket.version).toEqual(0);
+
+	// Update the ticket and save it
+	ticket.set({ price: 10 });
+	await ticket.save();
+
+	// Expect the version of the ticket to be 1
+	expect(ticket.version).toEqual(1);
+
+	// Update the ticket again and save it
+	ticket.set({ price: 15 });
+	await ticket.save();
+
+	// Expect the version of the ticket to be 2
+	expect(ticket.version).toEqual(2);
+});
