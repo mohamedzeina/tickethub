40a53b6c769c3e9913aa804b7a3bcd6c152f6a6b
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-20 16:11:05 +0300

    test(orders): Add TicketUpdatedListener tests

---
+++ orders/src/events/listeners/__tests__/ticket-updated-listener.test.ts
@@ -0,0 +1,60 @@
+import { TicketUpdatedListener } from '../ticket-updated-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { TicketUpdatedEvent } from '@zeina-tickethub/common';
+import { Message } from 'node-nats-streaming';
+import mongoose from 'mongoose';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+	// Create an instance of the listener
+	const listener = new TicketUpdatedListener(natsWrapper.client);
+
+	// Create and save a ticket
+	const ticket = Ticket.build({
+		id: new mongoose.Types.ObjectId().toHexString(),
+		title: 'Akon Concert',
+		price: 100,
+	});
+
+	await ticket.save();
+
+	// Create a fake data event
+	const data: TicketUpdatedEvent['data'] = {
+		version: ticket.version + 1,
+		id: ticket.id,
+		title: 'Akon Concert',
+		price: 10,
+		userId: new mongoose.Types.ObjectId().toHexString(),
+	};
+
+	// Create a fake message object
+	// @ts-ignore
+	const msg: Message = {
+		ack: jest.fn(),
+	};
+
+	return { listener, data, ticket, msg };
+};
+
+it('finds, updates, and saves a ticket', async () => {
+	const { listener, data, ticket, msg } = await setup();
+
+	// Call the onMessage function with the data and message object
+	await listener.onMessage(data, msg);
+
+	// Write assertions to make sure a ticket was updated
+	const updatedTicket = await Ticket.findById(ticket.id);
+
+	expect(updatedTicket).toBeDefined();
+	expect(updatedTicket!.title).toEqual(data.title);
+	expect(updatedTicket!.price).toEqual(data.price);
+	expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it('acks the message', async () => {
+	const { msg, data, listener } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(msg.ack).toHaveBeenCalled();
+});
