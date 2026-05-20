96b3f8013c6696f0972f6d3ebb2c4a2b095db1bd
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-20 15:25:09 +0300

    refactor(orders): Add Ticket.findByEvent helper

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -16,6 +16,10 @@ export interface TicketDoc extends mongoose.Document {
 }
 interface TicketModel extends mongoose.Model<TicketDoc> {
 	build(attrs: TicketAttrs): TicketDoc;
+	findByEvent(event: {
+		id: string;
+		version: number;
+	}): Promise<TicketDoc | null>;
 }
 
 interface TicketJSON {
@@ -55,6 +59,13 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
 		_id: attrs.id,
 		title: attrs.title,
 		price: attrs.price,
+	});
+};
+
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+	return Ticket.findOne({
+		_id: event.id,
+		version: event.version - 1,
 	});
 };
 
