2ec9e64f831e5a03addd6f4bcb735f2e5e0a1ca3
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-21 20:44:49 +0300

    test(orders): Add ExpirationCompleteListener tests

--- orders/src/events/listeners/__tests__/expiration-complete-listener.ts
+++ orders/src/events/listeners/__tests__/expiration-complete-listener.ts
@@ -37,3 +37,35 @@ const setup = async () => {
 
 	return { listener, order, ticket, data, msg };
 };
+
+it('updates the order status to canceled', async () => {
+	const { listener, order, data, msg } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	const updatedOrder = await Order.findById(order.id);
+
+	expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('emits an OrderCancelled event', async () => {
+	const { listener, order, data, msg } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+	const eventData = JSON.parse(
+		(natsWrapper.client.publish as jest.Mock).mock.calls[0][1],
+	);
+
+	expect(eventData.id).toEqual(order.id);
+});
+
+it('acks the message', async () => {
+	const { listener, data, msg } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(msg.ack).toHaveBeenCalled();
+});
