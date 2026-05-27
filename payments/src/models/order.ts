291ee0c5132d3aaa655db671a8aa860b4c169003
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-27 23:14:33 +0300

    Add Order model

---
+++ payments/src/models/order.ts
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@zeina-tickethub/common';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+interface OrderAttrs {
+	id: string;
+	status: OrderStatus;
+	version: number;
+	userId: string;
+	price: number;
+}
+
+interface OrderDoc extends mongoose.Document {
+	status: OrderStatus;
+	version: number;
+	userId: string;
+	price: number;
+}
+
+// Interface for the JSON representation after transformation
+interface OrderJSON {
+	status: string;
+	_id?: mongoose.Types.ObjectId; // Optional for deletion
+	price: number;
+	userId: string;
+	id?: string; // Added during transformation
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+	build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		status: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		toJSON: {
+			transform(doc: OrderDoc, ret: OrderJSON) {
+				ret.id = ret._id?.toString();
+				delete ret._id;
+			},
+		},
+	},
+);
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+	return new Order({
+		_id: attrs.id,
+		version: attrs.version,
+		price: attrs.price,
+		userId: attrs.userId,
+		status: attrs.status,
+	});
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
