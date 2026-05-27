c55475e58a4d54fe6f12a0c137363ec263f819e0
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-28 00:11:14 +0300

    feat(payments): Register order listeners on startup

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/order-cancelled-listener';
 
 const startTicketsService = async () => {
 	if (!process.env.JWT_KEY) {
@@ -34,6 +36,9 @@ const startTicketsService = async () => {
 
 		process.on('SIGINT', () => natsWrapper.client.close());
 		process.on('SIGTERM', () => natsWrapper.client.close());
+
+		new OrderCreatedListener(natsWrapper.client).listen();
+		new OrderCancelledListener(natsWrapper.client).listen();
 
 		await mongoose.connect(process.env.MONGO_URI);
 		console.log('Connected to Tickets MongoDB');
