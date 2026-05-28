ed4a44dc2927007c3ca518c2653e1366dd82397a
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-28 19:45:31 +0300

    feat(payments): Publish payment-created event after charge

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -5,13 +5,14 @@ import {
 	validateRequest,
 	BadRequestError,
 	NotFoundError,
-	currentUser,
 	NotAuthorizedError,
 	OrderStatus,
 } from '@zeina-tickethub/common';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
 import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -50,7 +51,13 @@ router.post(
 
 		await payment.save();
 
-		res.status(201).send({ success: true });
+		await new PaymentCreatedPublisher(natsWrapper.client).publish({
+			id: payment.id,
+			orderId: payment.orderId,
+			stripeId: payment.stripeId,
+		});
+
+		res.status(201).send({ id: payment.id });
 	},
 );
 
