f9e9ce04b28e695558c651ab75f99d50c5757f2b
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-28 20:00:13 +0300

    feat(orders): Mark order complete on payment-created event

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,30 @@
+import {
+	Listener,
+	PaymentCreatedEvent,
+	Subjects,
+	OrderStatus,
+} from '@zeina-tickethub/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+	readonly subject = Subjects.PaymentCreated;
+	queueGroupName: string = queueGroupName;
+
+	async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
+		const order = await Order.findById(data.orderId);
+
+		if (!order) {
+			throw new Error('Order not found');
+		}
+
+		order.set({
+			status: OrderStatus.Complete,
+		});
+
+		await order.save();
+
+		msg.ack();
+	}
+}
