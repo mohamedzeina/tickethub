c7eb91d8a40d45517250609004c57727af627d01
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-28 17:42:43 +0300

    feat(payments): Add Payment model

---
+++ payments/src/models/payment.ts
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+interface PaymentAttrs {
+	orderId: string;
+	stripeId: string;
+}
+
+interface PaymentDoc extends mongoose.Document {
+	orderId: string;
+	stripeId: string;
+}
+
+// Interface for the JSON representation after transformation
+interface PaymentJSON {
+	_id?: mongoose.Types.ObjectId;
+	orderId: string;
+	stripeId: string;
+	id?: string;
+}
+
+interface PaymentModel extends mongoose.Model<PaymentDoc> {
+	build(attrs: PaymentAttrs): PaymentDoc;
+}
+
+const paymentSchema = new mongoose.Schema(
+	{
+		orderId: {
+			required: true,
+			type: String,
+		},
+		stripeId: {
+			required: true,
+			type: String,
+		},
+	},
+	{
+		toJSON: {
+			transform(doc: PaymentDoc, ret: PaymentJSON) {
+				((ret.id = ret._id?.toString()), delete ret._id);
+			},
+		},
+	},
+);
+
+paymentSchema.statics.build = (attrs: PaymentAttrs) => {
+	return new Payment(attrs);
+};
+
+const Payment = mongoose.model<PaymentDoc, PaymentModel>(
+	'Payment',
+	paymentSchema,
+);
+
+export { Payment };
