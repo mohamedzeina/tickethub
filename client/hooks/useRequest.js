2ea34eb0173e3099bafeea14d3163c98e58a32a3
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-29 20:16:09 +0300

    feat(client): Submit payment token to payments API

--- client/hooks/useRequest.js
+++ client/hooks/useRequest.js
@@ -4,9 +4,9 @@ import { useState } from 'react';
 const useRequest = ({ url, method, body, onSuccess }) => {
 	const [errors, setErrors] = useState([]);
 
-	const doRequest = async () => {
+	const doRequest = async (props = {}) => {
 		try {
-			const response = await axios[method](url, body);
+			const response = await axios[method](url, { ...body, ...props });
 
 			if (onSuccess) {
 				onSuccess(response.data);
