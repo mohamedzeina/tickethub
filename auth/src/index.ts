b365b41e74155a4fa0fc65376830ff85f5b5e6ef
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-30 15:05:55 +0300

    Testing ci/cd after adding path

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const startAuthService = async () => {
-	console.log('testing ci/cd');
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
 	}
